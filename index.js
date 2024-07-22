446f7e0b3f2a21526da2f3933fabf1f5eb2ad99a
Taleeha <taleeha789@gmail.com>
2024-07-22 10:07:25 +0500

    updated vercel.json file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import usersRoutes from './routes/usersRoute.js';
 import errorHandler from "./middleware/errorHandler.js";
 import connectDb from "./config/dbConnection.js";
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const port = process.env.PORT || 5001;
 app.use(bodyParser.json());
 app.use(errorHandler);
 app.use('/users', usersRoutes);
+app.use(cors({"origins":"*"}));
 
 
 app.listen(port, () => console.log(`Server running on port: http://localhost:${port}`));
